4bf23dbe07863562beeea0b1cb97999fd2002910
Sudath Jayakodi <jssudathjayakodi@gmail.com>
2018-04-01 08:33:32 +0530

    twitter project
    twitter back end and front end used node js and angular 5

---
+++ api/config.js
@@ -0,0 +1,7 @@
+var appsettings = {
+    consumerkey: '2JLqJFxYsw91LItNBobDeCPxg',
+    consumersecret: 'DyOaUhNH4cKzjCBHBDdvsH0xQ6WvINJA42MnUyVT569laueqdj',
+    bearertoken: ''
+};
+
+module.exports = appsettings;
--- config.js
+++
@@ -1,7 +0,0 @@
-var appsettings = {
-    consumerkey: 'f3kQpzIik0ObMLUKZ4JbU4LgJ',
-    consumersecret: 'iIAKwoz0I0laVTK8QMmWm5utZUKs7LYifOOrqV46SDSQyLZ0Ze',
-    bearertoken: ''
-};
-
-module.exports = appsettings;
