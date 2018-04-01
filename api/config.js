dcb8ea396644cd9c1490edfb84817e598c95547c
Sudath Jayakodi <jssudathjayakodi@gmail.com>
2018-04-01 08:45:57 +0530

    ddd

--- api/config.js
+++
@@ -1,7 +0,0 @@
-var appsettings = {
-    consumerkey: '2JLqJFxYsw91LItNBobDeCPxg',
-    consumersecret: 'DyOaUhNH4cKzjCBHBDdvsH0xQ6WvINJA42MnUyVT569laueqdj',
-    bearertoken: ''
-};
-
-module.exports = appsettings;
